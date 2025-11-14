0e55a090a1002c6edf1e6a94a6c93e8ee9bd7d4b
Viktor Medvid <darkissdark@gmail.com>
2025-11-14 20:11:31 +0100

    refactor(auth): change refresh token requests from GET to POST for improved security

--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: NextRequest) {
     const next = request.nextUrl.searchParams.get("next") || "/";
 
     if (refreshToken) {
-      const apiRes = await api.get("auth/refresh", {
+      const apiRes = await api.post("auth/refresh", {
         headers: {
           Cookie: cookieStore.toString(),
         },
