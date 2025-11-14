393c057c8f642028cce86022f3faaa0c7dd52075
Viktor Medvid <darkissdark@gmail.com>
2025-11-15 00:30:04 +0100

    refactor(api): update baseURL format for consistency

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://taste-of-the-end-b.onrender.com/",
-  // baseURL: "http://localhost:3001/",
+  baseURL: "https://taste-of-the-end-b.onrender.com",
+  // baseURL: "http://localhost:3001",
   withCredentials: true,
 });
