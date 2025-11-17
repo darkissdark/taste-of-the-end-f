d877b0d2f063d99289f1b88c7b4ef4865b3de0d8
kar <myspecspace@gmail.com>
2025-11-17 20:59:46 +0400

    do not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,21 @@
-export default function NotFound() {
+import css from './not-found.module.css';
+import { SvgIcon } from '@/components/ui/icons/SvgIcon';
+import Link from 'next/link';
+
+const NotFound = () => {
   return (
-    <section style={{ padding: 24 }}>
-      <h1>Page not found</h1>
-      <p>The page you’re looking for doesn’t exist.</p>
-    </section>
+    <div className={css.pageWrapper}>
+      <div className={css.content}>
+        <h1 className={css.title}>Oops!</h1>
+        <p className={css.discription}>You are lost</p>
+        <SvgIcon name="img-404" className={css.logo} />
+        <button className={css.btn}>
+          <SvgIcon name="arrow-back" className={css.iconBack} />
+          <Link href="/">Go Home</Link>
+        </button>
+      </div>
+    </div>
   );
-}
+};
+
+export default NotFound;
