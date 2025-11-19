c76193a785ea7efbe68806dbdef0269c038b2f0b
Yurii <voronich97@gmail.com>
2025-11-19 17:14:54 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,6 @@ import { pageMeta } from '@/lib/seo';
 
 export const generateMetadata = () =>
   pageMeta({ title: 'Home', description: 'Browse all recipes' });
-export default async function Page() {
-  const data = await fetchRecipes();
-  return (
-    <section>
-      Головна сторінка
-      <RecipesList data={data} />
-    </section>
-  );
+export default function Page() {
+  return <section>Головна сторінка</section>;
 }
