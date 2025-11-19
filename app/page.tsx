de54e076307ccae8bdaf8c4f35085e617a72a483
Yurii <voronich97@gmail.com>
2025-11-19 17:12:01 +0200

    testing auth logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import { pageMeta } from "@/lib/seo";
+import RecipesList from '@/components/recipes/RecipesList/RecipesList';
+import { fetchRecipes } from '@/lib/api/clientApi';
+import { pageMeta } from '@/lib/seo';
 
 export const generateMetadata = () =>
-  pageMeta({ title: "Home", description: "Browse all recipes" });
-
-export default function Page() {
-  return <section>Головна сторінка</section>;
+  pageMeta({ title: 'Home', description: 'Browse all recipes' });
+export default async function Page() {
+  const data = await fetchRecipes();
+  return (
+    <section>
+      Головна сторінка
+      <RecipesList data={data} />
+    </section>
+  );
 }
